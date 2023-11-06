e6c42c9ba450ac2d7c5e33e2aacc15828b8186be
HibathRahman <147071016+HibathRahman@users.noreply.github.com>
2023-11-06 11:52:00 +0530

    Add files via upload

---
+++ redis.js
@@ -0,0 +1,29 @@
+const redis = require('redis');
+const dotenv = require('dotenv')
+dotenv.config();
+
+
+// byDefault it run on the localhost port 6379
+const redisClient = () =>{
+      return redis.createClient()
+} 
+
+const client = redisClient();
+// these are listeners
+client.on("error", (err) => {
+    console.log(err);
+})
+
+client.on("connect" , () => {
+    console.log("connected to redis");
+})
+
+client.on("end" , () => {
+    console.log("redis connection ended");
+})
+
+client.on("SIGIQUIT" , () => {
+  client.quit()
+})
+
+module.exports = {client};
